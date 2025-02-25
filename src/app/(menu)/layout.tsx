66bc931dc251da48a1e897c132a38f68325b43c5
Mahusaa <usamah.hafizh@ui.ac.id>
2025-02-25 16:24:38 +0700

    [fix] add returning

--- src/app/(menu)/layout.tsx
+++ src/app/(menu)/layout.tsx
@@ -7,19 +7,20 @@ import {
 import { AppSidebar } from "~/components/app-sidebar"
 
 
-export default function MenuLayout({
+export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  <SidebarProvider>
-    <AppSidebar />
-    <SidebarInset>
-      <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-        <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mr-2 h-4" />
-        Pasar Rakyat 2.0
-      </header>
-      {children}
-    </SidebarInset>
-  </SidebarProvider>
-
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          Pasar Rakyat 2.0
+        </header>
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  )
 }
